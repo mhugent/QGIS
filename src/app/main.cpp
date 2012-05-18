017dc4bdff516f59f508cbee353c5beabde37014
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2012-05-18 09:10:55 +0200

    Add nqmap splash screen

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -589,7 +589,7 @@ int main( int argc, char *argv[] )
 
   //set up splash screen
   QString mySplashPath( QgsApplication::splashPath() );
-  QPixmap myPixmap( mySplashPath + QString( "splash.png" ) );
+  QPixmap myPixmap( mySplashPath + QString( "nqmap_splash.jpg" ) );
   QSplashScreen *mypSplash = new QSplashScreen( myPixmap );
   if ( mySettings.value( "/qgis/hideSplash" ).toBool() || myHideSplash )
   {
