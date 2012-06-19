1d801d69c07f832dc55713eb16b0307768b75c9c
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2012-06-19 16:19:11 +0200

    More renaming

---
+++ src/plugins/web_data/webdataplugin.h
@@ -0,0 +1,28 @@
+#ifndef WEBDATAPLUGIN_H
+#define WEBDATAPLUGIN_H
+
+#include "qgisplugin.h"
+#include <QObject>
+
+class QgisInterface;
+class QAction;
+
+class WebDataPlugin: public QObject, public QgisPlugin
+{
+    Q_OBJECT
+  public:
+    WebDataPlugin( QgisInterface* iface );
+    ~WebDataPlugin();
+
+    void initGui();
+    void unload();
+
+  private slots:
+    void showWebDataDialog();
+
+  private:
+    QgisInterface* mIface;
+    QAction* mAction;
+};
+
+#endif // WEBDATAPLUGIN_H
