3aa4b50525821b791cf7f1853d53bfc4172d565c
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2012-07-20 09:57:39 +0200

    Add license header for html item

--- src/core/composer/qgscomposerhtmlitem.cpp
+++ src/core/composer/qgscomposerhtmlitem.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+                           qgscomposerhtmlitem.cpp
+                             -------------------
+    begin                : Juli 2012
+    copyright            : (C) 2012 by Marco Hugentobler
+    email                : marco.hugentobler@sourcepole.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgscomposerhtmlitem.h"
 #include <QApplication>
 #include <QWebFrame>
@@ -7,7 +24,7 @@ QgsComposerHtmlItem::QgsComposerHtmlItem( QgsComposition* c ): QgsComposerItem( 
   //test
   mHtml = new QWebPage();
   QObject::connect( mHtml, SIGNAL( loadFinished( bool ) ), this, SLOT( frameLoaded( bool ) ) );
-  //setUrl( QUrl( "http://www.server.com" ) );
+ // setUrl( QUrl( "http://www.myserver.com" ) );
 }
 
 QgsComposerHtmlItem::~QgsComposerHtmlItem()
@@ -29,7 +46,7 @@ void QgsComposerHtmlItem::setUrl( const QUrl& url )
 void QgsComposerHtmlItem::paint( QPainter* painter, const QStyleOptionGraphicsItem* itemStyle, QWidget* pWidget )
 {
   drawBackground( painter );
-  mHtml->mainFrame()->render( painter );
+  mHtml->mainFrame()->render( painter, QRegion( 0, 0, rect().width(), rect().height() ) );
   drawFrame( painter );
   if ( isSelected() )
   {
--- src/core/composer/qgscomposerhtmlitem.h
+++ src/core/composer/qgscomposerhtmlitem.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+                           qgscomposerhtmlitem.h
+                             -------------------
+    begin                : Juli 2012
+    copyright            : (C) 2012 by Marco Hugentobler
+    email                : marco.hugentobler@sourcepole.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef QGSCOMPOSERHTMLITEM_H
 #define QGSCOMPOSERHTMLITEM_H
 
