3d8006f2c4c6930276ea3d5cf36112b863ca1a50
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2013-06-06 14:28:45 +0200

    [FEATURE]: Add sensor observation service plugin

---
+++ src/plugins/sos/qgssosplugin.cpp
@@ -0,0 +1,106 @@
+/***************************************************************************
+                          qgssosplugin.cpp  -  description
+                          ------------------------------
+    begin                : June 2013
+    copyright            : (C) 2013 by Marco Hugentobler
+    email                : marco dot hugentobler at sourcepole dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgssosplugin.h"
+#include "qgis.h"
+#include "qgisinterface.h"
+#include <QAction>
+
+static const QString name_ = QObject::tr( "SOS plugin" );
+static const QString description_ = QObject::tr( "A plugin to access sensor observation services" );
+static const QString version_ = QObject::tr( "Version 0.1" );
+//static const QString icon_ = ":/raster/dem.png";
+static const QString category_ = QObject::tr( "Vector" );
+
+QgsSOSPlugin::QgsSOSPlugin( QgisInterface* iface ): mIface( iface ), mAction( 0 )
+{
+
+}
+
+QgsSOSPlugin::~QgsSOSPlugin()
+{
+  unload();
+}
+
+void QgsSOSPlugin::initGui()
+{
+  if ( mIface )
+  {
+    mAction = new QAction( tr( "Sensor observation service" ), 0 );
+    QObject::connect( mAction, SIGNAL( triggered() ), this, SLOT( showSOSDialog() ) );
+    mIface->addVectorToolBarIcon( mAction );
+    mIface->addPluginToVectorMenu( tr( "Sensor ovservation service" ), mAction );
+  }
+}
+
+void QgsSOSPlugin::unload()
+{
+  if ( mAction && mIface )
+  {
+    mIface->removeVectorToolBarIcon( mAction );
+    mIface->removePluginVectorMenu( tr( "Sensor ovservation service" ), mAction );
+  }
+  delete mAction;
+  mAction = 0;
+}
+
+void QgsSOSPlugin::showSOSDialog()
+{
+  qWarning( "Show SOS dialog" );
+}
+
+//global methods for the plugin manager
+QGISEXTERN QgisPlugin* classFactory( QgisInterface * ifacePointer )
+{
+  return new QgsSOSPlugin( ifacePointer );
+}
+
+QGISEXTERN QString name()
+{
+  return name_;
+}
+
+QGISEXTERN QString description()
+{
+  return description_;
+}
+
+QGISEXTERN QString version()
+{
+  return version_;
+}
+
+/*QGISEXTERN QString icon()
+{
+  return icon_;
+}*/
+
+QGISEXTERN int type()
+{
+  return QgisPlugin::UI;
+}
+
+QGISEXTERN void unload( QgisPlugin* pluginPointer )
+{
+  delete pluginPointer;
+}
+
+QGISEXTERN QString category()
+{
+  return category_;
+}
+
