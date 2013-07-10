c7abd71f92d59c0c5a9ff3a2c81ddaac7eb1223e
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2013-07-10 13:03:59 +0200

    Add metadata file for survey plugin

--- python/plugins/survey_design/__init__.py
+++ python/plugins/survey_design/__init__.py
@@ -1,20 +1,4 @@
 from surveydesignplugin import SurveyDesignPlugin
 
-def name():
-    return "Survey design plugin"
-
-def description():
-    return "A plugin to design and evaluate biomass surveys"
-
-def version():
-    return "0.1"
-
-def qgisMinimumVersion():
-    return "1.0.0"
-
 def classFactory(iface):
     return SurveyDesignPlugin( iface )
-
-#def icon():
-
-#def category():
